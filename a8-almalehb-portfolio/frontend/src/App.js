f2649587724c34dc0ff16f5753bb66a117200abe
almalehb <102708971+almalehb@users.noreply.github.com>
2023-08-12 21:03:07 -0700

    add src

---
+++ a8-almalehb-portfolio/frontend/src/App.js
@@ -0,0 +1,57 @@
+// Import dependencies
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import Components, styles, media
+import Navigation from './components/Navigation';
+import './App.css';
+
+// Import Pages (but only the ones you need).
+// For Create and Edit, use the form OR table design; not both.
+// If your schema requires LONG data input, then use the FORM design.
+// If your schema requires SHORT data input, then use the TABLE design.
+
+import MoviesPage from './pages/MoviesPage';
+import AddMoviePageForm from './pages/AddMoviePageForm';
+import AddMoviePageTable from './pages/AddMoviePageTable';
+import EditMoviePageForm from './pages/EditMoviePageForm';
+import EditMoviePageTable from './pages/EditMoviePageTable';
+
+// Define the function that renders the content in Routes, using State.
+function App() {
+
+  const [movie, setMovieToEdit] = useState([])
+
+  return (
+    <>
+      <BrowserRouter>
+
+          <header>
+            <h1>Movie Collection</h1>
+            <p>Describe this collection.</p>
+          </header>
+
+          <Navigation />
+
+          <main>
+            <section>
+                <Routes> 
+                    <Route path="/" element={<MoviesPage setMovie={setMovieToEdit}/>} />
+                    {/* <Route path="/create" element={<AddMoviePageForm />} />  */}
+                    <Route path="/create" element={<AddMoviePageTable />} /> 
+                    {/* <Route path="/update" element={<EditMoviePageForm movieToEdit={movie} />} /> */}
+                    <Route path="/update" element={<EditMoviePageTable movieToEdit={movie} />} />
+                </Routes>
+              </section>
+          </main>
+
+          <footer>
+            <p>Copyright statement</p>
+          </footer>
+
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
