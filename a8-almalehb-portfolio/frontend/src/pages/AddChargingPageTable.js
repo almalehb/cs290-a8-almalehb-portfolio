485b5921fa556b03a593e5f900059ecad8713f16
almalehb <102708971+almalehb@users.noreply.github.com>
2023-08-13 14:04:17 -0700

    add time units

--- a8-almalehb-portfolio/frontend/src/pages/AddChargingPageTable.js
+++ a8-almalehb-portfolio/frontend/src/pages/AddChargingPageTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
 // Change the icons, function names, and parameters 
@@ -8,15 +8,31 @@ export const AddChargingPageTable = () => {
 
     const [time, setTime] = useState(Date());
     const [durationInSeconds, setDurationInSeconds] = useState(0);
+
+    const [duration, setDuration] = useState(0);
+    const [durationUnits, setDurationUnits] = useState('');
+
     const [address, setAddress] = useState('');
     const [kwh, setKwh] = useState(0);
     const [pricePerKwh, setPricePerKwh] = useState(0);
-    
+
     const redirect = useNavigate();
+
+    useEffect(() => {
+        let durationResult = 0; 
+        if (durationUnits === "hours") { 
+            durationResult = duration * 60 * 60
+        } else if (durationUnits === "minutes") {
+            durationResult = duration * 60
+        } else { 
+            durationResult = duration
+        }
+        setDurationInSeconds(durationResult);
+    }, [durationUnits, duration]);
 
     const addChargingSession = async () => {
 
-        const totalPrice = durationInSeconds * pricePerKwh
+        const totalPrice = durationInSeconds * pricePerKwh;
 
         const newChargingSession = { time, durationInSeconds, address, kwh };
         const response = await fetch('/chargingSessions', {
@@ -26,92 +42,100 @@ export const AddChargingPageTable = () => {
                 'Content-Type': 'application/json',
             },
         });
-        if(response.status === 201){
+        if (response.status === 201) {
             alert(`Successfully added your new charging session at ${address}!`);
         } else {
             alert(`We were unable to add your charging session. We received error code = ${response.status}`);
         }
-        redirect("/");
     };
 
     return (
         <>
-        <article>
-            <h2>Add a charging session</h2>
-            <p>Tell us about your new charging session.</p>
-            
-            <table id="chargingSessions">
-                <caption>Enter the details for the session.</caption>
-                <thead>
-                    <tr>
-                        <th>Time</th>
-                        <th>Duration (Seconds)</th>
-                        <th>Address</th>
-                        <th>kWh</th>
-                        <th>Price per kWh</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <tr>
-                <td><label for="time">Charging Time</label>
-                        <input
-                            type="date"
-                            placeholder="Date and time of your charging session."
-                            value={time}
-                            onChange={e => setTime(e.target.value)} 
-                            id="time" />
-                    </td>
+            <article>
+                <h2>Add a charging session</h2>
+                <p>Tell us about your new charging session.</p>
+                <p>Current duration in seconds: {durationInSeconds}</p>
+                <table id="chargingSessions">
+                    <caption>Enter the details for the session.</caption>
+                    <thead>
+                        <tr>
+                            <th>Time</th>
+                            <th>Duration</th>
+                            <th>Address</th>
+                            <th>kWh</th>
+                            <th>Price per kWh</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input
+                                    type="date"
+                                    placeholder="Date and time of your charging session."
+                                    value={time}
+                                    onChange={e => setTime(e.target.value)}
+                                    id="time" />
+                            </td>
 
-                    <td><label for="duration">Charging Duration</label>
-                        <input
-                            type="number"
-                            value={durationInSeconds}
-                            placeholder="Duration in seconds."
-                            onChange={e => setDurationInSeconds(e.target.value)} 
-                            id="duration" />
-                    </td>
+                            <td>
+                                <div className="charge-duration-container">
+                                    <input
+                                        type="number"
+                                        value={duration}
+                                        min={0}
+                                        onChange={e => setDuration(e.target.value)}
+                                        className="charge-duration" />
+                                    <select className="time-unit-select" onChange={e => setDurationUnits(e.target.value)}> 
+                                        <option value="seconds">Seconds</option>
+                                        <option value="minutes">Minutes</option>
+                                        <option value="hours">Hours</option>
+                                    </select>
+                                </div>
+                            </td>
 
-                    <td><label for="Address">Address</label>
-                        <input
-                            type="text"
-                            placeholder="Location of the charging session."
-                            value={address}
-                            onChange={e => setAddress(e.target.value)} 
-                            id="Address" />
-                    </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    placeholder="Location of the charging session."
+                                    value={address}
+                                    onChange={e => setAddress(e.target.value)}
+                                    id="Address" />
+                            </td>
 
-                    <td><label for="kwh">kWh</label>
-                        <input
-                            type="number"
-                            placeholder="Kilowatt-hour amount."
-                            value={kwh}
-                            onChange={e => setKwh(e.target.value)} 
-                            id="kwh" />
-                    </td>
+                            <td>
+                                <input
+                                    type="number"
+                                    placeholder="Kilowatt-hour amount."
+                                    value={kwh}
+                                    min={0}
+                                    onChange={e => setKwh(e.target.value)}
+                                    id="kwh" />
+                            </td>
 
-                    <td><label for="pricePerKwh">Price Per kWh</label>
-                        <input
-                            type="number"
-                            placeholder="Price per kilowatt-hour."
-                            value={pricePerKwh}
-                            onChange={e => setPricePerKwh(e.target.value)} 
-                            id="pricePerKwh" />
-                    </td>
+                            <td>
+                                <input
+                                    type="number"
+                                    placeholder="Price per kilowatt-hour."
+                                    value={pricePerKwh}
+                                    min={0}
+                                    onChange={e => setPricePerKwh(e.target.value)}
+                                    id="pricePerKwh" />
+                            </td>
 
-                    <td>
-                    <label for="submit">Submit when ready</label>
-                        <button
-                            type="submit"
-                            onClick={addChargingSession}
-                            id="submit"
-                        >Add</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </article>
-    </>
-);
+                            <td>
+
+                                <button
+                                    type="submit"
+                                    onClick={addChargingSession}
+                                    id="submit"
+                                >Add</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </article>
+        </>
+    );
 }
 
 export default AddChargingPageTable;
--- a8-almalehb-portfolio/frontend/src/pages/EditChargingPageTable.js
+++ a8-almalehb-portfolio/frontend/src/pages/EditChargingPageTable.js
@@ -1,27 +1,43 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
 export const EditChargingPageTable = ({ chargingSessionToEdit }) => {
- 
+
     const [time, setTime] = useState(chargingSessionToEdit.time);
     const [durationInSeconds, setDurationInSeconds] = useState(chargingSessionToEdit.durationInSeconds);
+
+    const [duration, setDuration] = useState(0);
+    const [durationUnits, setDurationUnits] = useState('');
+
     const [address, setAddress] = useState(chargingSessionToEdit.address);
     const [kwh, setKwh] = useState(chargingSessionToEdit.kwh);
     const [pricePerKwh, setPricePerKwh] = useState(chargingSessionToEdit.pricePerKwh);
-    
+
     const redirect = useNavigate();
+
+    useEffect(() => {
+        let durationResult = 0; 
+        if (durationUnits === "hours") { 
+            durationResult = duration * 60 * 60
+        } else if (durationUnits === "minutes") {
+            durationResult = duration * 60
+        } else { 
+            durationResult = duration
+        }
+        setDurationInSeconds(durationResult);
+    }, [durationUnits, duration]);
 
     const editChargingSession = async () => {
         const response = await fetch(`/chargingSessions/${chargingSessionToEdit._id}`, {
             method: 'PUT',
-            body: JSON.stringify({ 
-                time: time, 
+            body: JSON.stringify({
+                time: time,
                 durationInSeconds: durationInSeconds,
-                address: address, 
-                kwh: kwh, 
+                address: address,
+                kwh: kwh,
                 pricePerKwh: pricePerKwh
             }),
-            headers: {'Content-Type': 'application/json',},
+            headers: { 'Content-Type': 'application/json', },
         });
 
         if (response.status === 200) {
@@ -30,82 +46,90 @@ export const EditChargingPageTable = ({ chargingSessionToEdit }) => {
             const errMessage = await response.json();
             alert(`We were unable to update your charging session. We received code = ${response.status}. ${errMessage.Error}`);
         }
-        redirect("/");
     }
 
     return (
         <>
-        <article>
-            <h2>Edit a charging session</h2>
-            <p>Modify any of the properties of an existing session.</p>
-            <table id="chargingSessions">
-                <caption>What changes would you like to make?</caption>
-                <thead>
-                    <tr>
-                        <th>Time</th>
-                        <th>Duration (Seconds)</th>
-                        <th>Address</th>
-                        <th>kWh</th>
-                        <th>Price per kWh</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <tr>
-                <td><label for="time">Charging Time</label>
-                        <input
-                            type="date"
-                            placeholder={chargingSessionToEdit.time}
-                            value={time}
-                            onChange={e => setTime(e.target.value)} 
-                            id="time" />
-                    </td>
+            <article>
+                <h2>Edit a charging session</h2>
+                <p>Modify any of the properties of an existing session.</p>
+                <table id="chargingSessions">
+                    <caption>What changes would you like to make?</caption>
+                    <thead>
+                        <tr>
+                            <th>Time</th>
+                            <th>Duration</th>
+                            <th>Address</th>
+                            <th>kWh</th>
+                            <th>Price per kWh</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input
+                                    type="date"
+                                    placeholder={chargingSessionToEdit.time}
+                                    value={time}
+                                    onChange={e => setTime(e.target.value)}
+                                    id="time" />
+                            </td>
 
-                    <td><label for="duration">Charging Duration</label>
-                        <input
-                            type="number"
-                            value={durationInSeconds}
-                            placeholder={chargingSessionToEdit.durationInSeconds}
-                            onChange={e => setDurationInSeconds(e.target.value)} 
-                            id="duration" />
-                    </td>
+                            <td>
+                                <div className="charge-duration-container">
+                                    <input
+                                        type="number"
+                                        value={duration}
+                                        min={0}
+                                        onChange={e => setDuration(e.target.value)}
+                                        className="charge-duration" />
+                                    <select className="time-unit-select" onChange={e => setDurationUnits(e.target.value)}>
+                                        <option value="seconds">Seconds</option>
+                                        <option value="minutes">Minutes</option>
+                                        <option value="hours">Hours</option>
+                                    </select>
+                                </div>
+                            </td>
 
-                    <td><label for="Address">Address</label>
-                        <input
-                            type="text"
-                            placeholder={chargingSessionToEdit.address}
-                            value={address}
-                            onChange={e => setAddress(e.target.value)} 
-                            id="Address" />
-                    </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    placeholder={chargingSessionToEdit.address}
+                                    value={address}
+                                    onChange={e => setAddress(e.target.value)}
+                                    id="Address" />
+                            </td>
 
-                    <td><label for="kwh">kWh</label>
-                        <input
-                            type="number"
-                            placeholder={chargingSessionToEdit.kwh}
-                            value={kwh}
-                            onChange={e => setKwh(e.target.value)} 
-                            id="kwh" />
-                    </td>
+                            <td>
+                                <input
+                                    type="number"
+                                    placeholder={chargingSessionToEdit.kwh}
+                                    value={kwh}
+                                    min={0}
+                                    onChange={e => setKwh(e.target.value)}
+                                    id="kwh" />
+                            </td>
 
-                    <td><label for="pricePerKwh">Price Per kWh</label>
-                        <input
-                            type="number"
-                            placeholder={chargingSessionToEdit.pricePerKwh}
-                            value={pricePerKwh}
-                            onChange={e => setPricePerKwh(e.target.value)} 
-                            id="pricePerKwh" />
-                    </td>
-                    <td>
-                    <label for="submit">Commit</label>
-                        <button
-                            type="submit"
-                            onClick={editChargingSession}
-                            id="submit"
-                        >Edit</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                            <td>
+                                <input
+                                    type="number"
+                                    placeholder={chargingSessionToEdit.pricePerKwh}
+                                    value={pricePerKwh}
+                                    min={0}
+                                    onChange={e => setPricePerKwh(e.target.value)}
+                                    id="pricePerKwh" />
+                            </td>
+                            <td>
+
+                                <button
+                                    type="submit"
+                                    onClick={editChargingSession}
+                                    id="submit"
+                                >Edit</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </article>
         </>
     );
